f69c30575510d239c5705035fc158e5f2592f63a
Oleg Steba <oleg.steba@gmail.com>
2024-07-29 14:09:05 +0500

    Fixed docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.93.79', '127.0.0.1',
+                 'localhost', 'ole-taski.zapto.org']
 
 
 # Application definition
@@ -109,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
