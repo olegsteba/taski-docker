58b1a775b510c4205b17cf28c384d8313b254136
Oleg Steba <oleg.steba@gmail.com>
2024-07-31 15:07:42 +0500

    action flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.93.79', '127.0.0.1', 'localhost', 'ole-taski.zapto.org']
+ALLOWED_HOSTS = ['158.160.93.79', '127.0.0.1',
+                 'localhost', 'ole-taski.zapto.org']
 
 
 # Application definition
